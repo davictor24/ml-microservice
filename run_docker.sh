4234300ff1b908b483e6e00800dc8906430a363d
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-12 22:37:18 +0100

    Script for building Docker image and spinning up a new container

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t davictor/ml-microservice .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run --rm --name ml-microservice -p 80:80/tcp davictor/ml-microservice
